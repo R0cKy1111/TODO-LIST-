43e69eef3a7209a8df5dd1223bebedb8f8c5413f
R0cKy1111 <145274407+R0cKy1111@users.noreply.github.com>
2023-09-17 16:59:25 +0200

    index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		<link rel="stylesheet" type="text/css" href="http://todolist.great-site.net/css/bootstrap.css"/>
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
 	</head>
 <body>
 	
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
-		<h3 class="text-primary">PHP - Simple To Do List App</h3>
+		<h3 class="text-primary"><center>PHP - Simple To Do List App</center></h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
